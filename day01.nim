4aa4cb162f608de1d852b5d7a39ba9ee7c862966
Pietro Peterlongo <pietro.peterlongo@agilelab.com>
2024-12-02 00:27:52 +0100

    part1 solved

--- day01.nim
+++ day01.nim
@@ -1,9 +1,26 @@
+import batteries
 import jsony
 
+## parsing with possum
+# possum 01.possum input/01.txt > input/01.json
 type
   Pair = object
     left, right: int
-# possum 01.possum input/01.txt > input/01.json
 
+## reading into nim and transforming into two sequences
 let input = "input/01.json".readFile.fromJson(seq[Pair])
-echo input
+
+var left, right: seq[int]
+for pair in input:
+  left.add pair.left
+  right.add pair.right
+
+## sort the sequences
+sort left
+sort right
+
+var part1 = 0
+for i in left.low .. left.high:
+  part1 += abs(left[i] - right[i])
+
+dump part1
